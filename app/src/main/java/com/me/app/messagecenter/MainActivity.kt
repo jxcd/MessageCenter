e74767796f705888aa9b767d5b4aff450ccfa6c9
cwj <coderwj@live.com>
2023-07-02 11:46:41 +0800

    启动时检查权限, 无权限时申请

--- app/src/main/java/com/me/app/messagecenter/MainActivity.kt
+++ app/src/main/java/com/me/app/messagecenter/MainActivity.kt
@@ -1,8 +1,12 @@
 package com.me.app.messagecenter
 
+import android.Manifest
+import android.content.pm.PackageManager
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
@@ -10,11 +14,41 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import com.me.app.messagecenter.ui.theme.MessageCenterTheme
 
+const val PERMISSION_REQUEST_SMS = 1
+
 class MainActivity : ComponentActivity() {
+
+    private val requestPermissionLauncher =
+        registerForActivityResult(ActivityResultContracts.RequestPermission()) { isGranted: Boolean ->
+            if (isGranted) {
+                // RECEIVE_SMS 权限已授予，可以执行相应操作
+                // ...
+            } else {
+                // RECEIVE_SMS 权限被拒绝
+                Toast.makeText(this, "没有 RECEIVE_SMS 权限", Toast.LENGTH_SHORT).show()
+            }
+        }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.RECEIVE_SMS)
+            != PackageManager.PERMISSION_GRANTED
+        ) {
+            // 如果没有 RECEIVE_SMS 权限，则请求权限
+            ActivityCompat.requestPermissions(
+                this,
+                arrayOf(Manifest.permission.RECEIVE_SMS),
+                PERMISSION_REQUEST_SMS
+            )
+            // 如果没有 RECEIVE_SMS 权限，则请求权限
+            requestPermissionLauncher.launch(Manifest.permission.RECEIVE_SMS)
+        }
+
         setContent {
             MessageCenterTheme {
                 // A surface container using the 'background' color from the theme
